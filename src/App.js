aabab27b15f5d8439ac1a2386ce253f0accefbbe
Ivan Luis Jimenez <ILuis>
2021-03-02 08:45:54 -0600

    Adding proyect with style

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginsGetApi from './LoginsGetApi';
+
+function App() {
+  return (
+    <div>
+        <LoginsGetApi />
+    </div>
+  );
+}
+
+export default App;
